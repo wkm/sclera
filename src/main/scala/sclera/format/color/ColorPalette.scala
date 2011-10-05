f7de3c8aa5d4f4ba90d1296f7e4b3d81a616de64
Wiktor Macura <wmacura@gmail.com>
2011-10-05 01:38:36 -0600

    Support for hex colors in color palettes

--- src/main/scala/sclera/format/color/ColorPalette.scala
+++ src/main/scala/sclera/format/color/ColorPalette.scala
@@ -5,7 +5,18 @@ import java.awt.Color
 class ColorPalette {
   var colors = Map[String, Color]()
 
-  def color(name: String, r:Int, g:Int, b:Int) {
-    colors += name -> new Color(r, g, b)
+  def addHexColor(name: String, hex: String) {
+    addColor(name, Color.decode(hex.toUpperCase))
   }
+
+  def addRgbColor(name: String, r:Int, g:Int, b:Int) {
+    addColor(name, new Color(r, g, b))
+  }
+
+  def addColor(name: String, k: Color) {
+    colors += name -> k
+  }
+
+  def apply(name: String): Color =
+    return colors.get(name).getOrElse(Color.decode("d33682"))
 }
