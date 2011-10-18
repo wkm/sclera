1f0b9aac8947a1e204b158fba4734a92382d1ad9
Wiktor Macura <wmacura@gmail.com>
2011-10-18 10:32:23 -0400

    Fix freezing of IMain tests within SBT

--- src/test/scala/sclera/evaluator/IMainSpecs.scala
+++ src/test/scala/sclera/evaluator/IMainSpecs.scala
@@ -7,21 +7,21 @@ import sclera.ux.UXPad
 
 object IMainSpecs extends Specification {
   "IMain" should {
-    "give term value" in {
-      System.setProperty("settings.usejavacp.value", "true")
+    "should always give last request" in {
       val settings = new Settings()
-      settings.embeddedDefaults[UXPad]
-      settings.processArgumentString("-usejavacp")
+      settings.embeddedDefaults[Specification]
 
       class MyIMain extends IMain(settings) {
         def lastRequest = prevRequestList.last
       }
 
       val n = new MyIMain()
-      n.interpret("val a = 1")
+      n.interpret("val a = 1; val b = 2")
 
-      val r = n.lastRequest
-      r.lineRep.call("$result").must_== (1)
+      // this isn't ideal: we should be able to get the values of
+      // a and b separately
+      n.requestForIdent("a").get.lineRep.call("$result").must_== (2)
+      n.requestForIdent("b").get.lineRep.call("$result").must_== (2)
     }
   }
 }
