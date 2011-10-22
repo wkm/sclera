2003e5d782bc818004025820c1030c57ca91515a
Wiktor Macura <wmacura@gmail.com>
2011-10-21 22:29:34 -0400

    Enable some magical apple system properties to use beautiful font rendering

--- src/main/scala/sclera/Sclera.scala
+++ src/main/scala/sclera/Sclera.scala
@@ -10,6 +10,8 @@ import javax.swing.{UIManager, BorderFactory}
 
 object Sclera extends SimpleSwingApplication {
   System.setProperty("apple.laf.useScreenMenuBar", "true")
+  System.setProperty("apple.awt.textantialiasing", "true")
+  System.setProperty("apple.awt.graphics.UseQuartz", "true")
   System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Sclera")
   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
   
